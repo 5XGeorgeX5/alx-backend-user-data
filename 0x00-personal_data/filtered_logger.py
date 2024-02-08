4b00dfb90d626beab5427c67197c80366d8d66a4
George Raafat <georgexraafat@gmail.com>
2024-02-08 15:42:18 +0200

    doing some task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,6 @@
 from typing import List
 import re
 import logging
-import os
-from os import environ
 
 
 def filter_datum(
@@ -19,3 +17,24 @@ def filter_datum(
             f'{field}=[^{separator}]+',
             f'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
