0215e5e1dac9dff00d607a016a362bf893351639
George Raafat <georgexraafat@gmail.com>
2024-02-13 19:35:40 +0200

    doing some task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,11 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """checks the required path"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        return (path not in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """returns None"""
