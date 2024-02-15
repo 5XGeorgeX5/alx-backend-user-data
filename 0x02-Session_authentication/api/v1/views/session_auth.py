c656b4dbd78ca344a41bce2c16b4d90aab4acea1
George Raafat <georgexraafat@gmail.com>
2024-02-15 19:50:26 +0200

    doing some task

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -31,3 +31,12 @@ def session_login():
     session_name = getenv('SESSION_NAME')
     response.set_cookie(session_name, session_id)
     return response
+
+
+@app_views.route('auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """DELETE /api/v1/auth_session/logout"""
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({})
+    abort(404)
