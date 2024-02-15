ccd0a66a2cff414af22e97dd8fd4a62937aa086d
George Raafat <georgexraafat@gmail.com>
2024-02-15 18:53:57 +0200

    doing some task

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from typing import List, TypeVar
 from flask import request
+from os import getenv
 
 
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
@@ -35,3 +36,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = getenv("SESSION_NAME")
+        return request.cookies.get(session_name)
