ea60c200952621c332d0788a4a51d766d985ee4b
George Raafat <georgexraafat@gmail.com>
2024-02-15 20:08:50 +0200

    doing some task

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""class SessionExpAuth"""
+from api.v1.auth.session_auth import SessionAuth
+from os import getenv
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """class SessionExpAuth"""
+    def __init__(self):
+        """Initialize the class"""
+        self.session_duration = int(getenv('SESSION_DURATION', '0'))
+
+    def create_session(self, user_id=None):
+        """Overload the create session method"""
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        self.user_id_by_session_id[session_id] = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """Overload the user id for session id method"""
+        session = self.user_id_by_session_id.get(session_id)
+        if session is None:
+            return None
+        if self.session_duration <= 0:
+            return session.get('user_id')
+        created_at = session.get('created_at')
+        if not created_at:
+            return None
+        exp = created_at + timedelta(seconds=self.session_duration)
+        if exp < datetime.now():
+            return None
+        return session.get('user_id')
